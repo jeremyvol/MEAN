aa825c11ffca5c5c70d62f5682a0998734e26169
Jeremy VOLLANT <j.vollant@gmail.com>
2020-01-03 12:51:27 +0100

    first component

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html'
+})
+export class PostCreateComponent {
+  enteredValue = '';
+  newPost = 'NO CONTENT';
+
+  // onAddPost(postInput: HTMLTextAreaElement) {
+  //   console.dir(postInput);
+  //   this.newPost = postInput.value;
+  // }
+  onAddPost() {
+    this.newPost = this.enteredValue;
+  }
+}
