492be7967fec07b08bf37fc4e5fe043e3c5a077b
Jeremy VOLLANT <j.vollant@gmail.com>
2020-01-06 15:18:12 +0100

    Fisrt functionnal API

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
 
 import { Post } from './post.model';
@@ -8,8 +9,23 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
+
   getPosts() {
-    return [...this.posts];
+    this.http
+      .get<{ message: string; posts: Post[] }>(
+        'http://localhost:3000/api/posts'
+      )
+      .subscribe(
+        response => {
+          this.posts = response.posts;
+          this.postsUpdated.next([...this.posts]);
+        },
+        error => {
+          console.error(error);
+        },
+        () => {}
+      );
   }
 
   getPostsUpdateListener() {
@@ -17,7 +33,7 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    const post: Post = { title, content };
+    const post: Post = { id: null, title, content };
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
   }
