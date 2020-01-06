99442638e02b17091ae8da2a43e30a9bba4035ce
Jeremy VOLLANT <j.vollant@gmail.com>
2020-01-06 17:09:09 +0100

    test connection

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,22 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose
+  .connect(
+    'mongodb+srv://jeremyV:YG5laepaPDM1RPdN@cluster0-c2fum.mongodb.net/test?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,7 +36,11 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  // req.body;
   console.log(post);
   res.status(201).json({
     message: 'Post added successfully'
